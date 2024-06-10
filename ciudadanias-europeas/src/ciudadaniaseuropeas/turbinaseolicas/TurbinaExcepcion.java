9c184509c72189bedd72460311cccb7397ea474c
pHidalgo <pablohidalgo10@gmail.com>
2024-06-09 21:06:28 -0300

    Se modifican algunas carpetas.

--- ciudadanias-europeas/src/ciudadaniaseuropeas/turbinaseolicas/TurbinaExcepcion.java
+++
@@ -1,9 +0,0 @@
-package ciudadaniaseuropeas.turbinaseolicas;
-
-public class TurbinaExcepcion extends Exception {
-    public TurbinaExcepcion(String mensaje) {
-        super(mensaje);
-    }
-            
-}   
-   
